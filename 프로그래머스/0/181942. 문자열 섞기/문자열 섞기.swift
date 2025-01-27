87cd4c463d3a72fd8b1f61426cd47cb9c8bf4560
bae-suho <115385697+bae-suho@users.noreply.github.com>
2025-01-27 23:34:54 +0900

    [level 0] Title: 문자열 섞기, Time: 0.14 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.swift
+++ 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.swift
@@ -1,11 +1,7 @@
 import Foundation
 
 func solution(_ str1:String, _ str2:String) -> String {
-    var result = ""
-    let str1_arr = Array(str1)
-    let str2_arr = Array(str2)
-    for i in 0..<str1_arr.count { 
-        result += String(str1_arr[i]) + String(str2_arr[i])
-    }
+    var result = zip(str1, str2).map{String($0) + String($1)}.joined()
     return result
+    
 }
