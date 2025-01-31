39af2b2a22c1e8c2731d7e73f3b98575dfc65735
bae-suho <115385697+bae-suho@users.noreply.github.com>
2025-01-31 17:42:09 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.04 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.swift
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.swift
@@ -3,7 +3,7 @@ import Foundation
 func solution(_ num_list:[Int]) -> [Int] {
     var ret = num_list
     let last = ret[ret.count - 1]
-    let prevLast = ret[ret.count - 2]
-    ret.append(last > prevLast ? last - prevLast : last * 2)
+    let last2 = ret[ret.count - 2]
+    ret.append(last > last2 ? last - last2 : last * 2)
     return ret
 }
