44cc1d66e3e20e8679df5650ffc503f2c0609e2a
bae-suho <115385697+bae-suho@users.noreply.github.com>
2025-02-01 17:10:10 +0900

    [level 0] Title: 수 조작하기 2, Time: 49.25 ms, Memory: 22.2 MB -BaekjoonHub

--- 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.swift
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.swift
@@ -2,5 +2,5 @@ import Foundation
 
 func solution(_ numLog:[Int]) -> String {
     let op: [Int: String] = [1: "w", -1: "s", 10: "d", -10: "a"]
-    return (1...numLog.count-1).map{ op[numLog[$0] - numLog[$0 - 1]]! }.joined()
+    return (1..<numLog.count).map{ op[numLog[$0] - numLog[$0 - 1]]! }.joined()
 }
