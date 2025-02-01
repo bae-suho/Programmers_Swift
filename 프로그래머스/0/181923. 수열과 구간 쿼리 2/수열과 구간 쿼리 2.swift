1e101679db0db9ed07fe261fe49196739bd1622c
bae-suho <115385697+bae-suho@users.noreply.github.com>
2025-02-01 18:18:59 +0900

    [level 0] Title: 수열과 구간 쿼리 2, Time: 31.84 ms, Memory: 16.7 MB -BaekjoonHub

--- 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.swift
+++ 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.swift
@@ -1,16 +1,8 @@
 import Foundation
 
 func solution(_ arr:[Int], _ queries:[[Int]]) -> [Int] {
-    var result: [Int] = []
-    for q in queries {
-        var overGroup: [Int] = []
-        for i in q[0]...q[1] {
-            if arr[i] > q[2] {
-                overGroup.append(arr[i])
-            }
-        }
-        
-        result.append(overGroup.min() ?? -1)
+    return queries.map {
+        let (s, e, k) = ($0[0], $0[1], $0[2])
+        return arr[s...e].filter{ $0 > k }.min() ?? -1
     }
-    return result
 }
