2f141dec8d3151292e643d8ef8816b85b681b224
bae-suho <115385697+bae-suho@users.noreply.github.com>
2025-02-02 19:43:34 +0900

    [level 0] Title: 배열 만들기 2, Time: 2940.62 ms, Memory: 16.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+func solution(_ l:Int, _ r:Int) -> [Int] {
+    var ret: [Int] = []
+    for num in l...r{
+        let set = Set(String(num))
+        if set == Set(["0", "5"]) || set == Set(["5"]) || set == Set(["0"]) {
+            ret.append(num)
+        }
+    }
+    if ret == [] {
+        ret.append(-1)
+    }
+    return ret
+}
