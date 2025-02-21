d1da88830cfb7b460fbaa18a8471819df4e0a624
bae-suho <115385697+bae-suho@users.noreply.github.com>
2025-02-21 15:51:46 +0900

    [level 0] Title: 접미사 배열, Time: 2.74 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/0/181909. 접미사 배열/접미사 배열.swift
+++ 프로그래머스/0/181909. 접미사 배열/접미사 배열.swift
@@ -1,9 +1,13 @@
 import Foundation
 
-func solution(_ my_string:String) -> [String] {
-    var ret:[String] = []
-    for i in 0..<my_string.count{
-        ret.append(String(Array(my_string)[i...]))
-    }
-    return ret.sorted()
+// func solution(_ my_string:String) -> [String] {
+//     var ret:[String] = []
+//     for i in 0..<my_string.count{
+//         ret.append(String(Array(my_string)[i...]))
+//     }
+//     return ret.sorted()
+// }
+
+ func solution(_ my_string:String) -> [String] {
+    return (0..<my_string.count).map{ String(Array(my_string)[$0...]) }.sorted()
 }
