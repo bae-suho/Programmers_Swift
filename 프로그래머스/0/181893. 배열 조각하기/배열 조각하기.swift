af78946de08ad323f2abb5483153e47be7756ede
bae-suho <115385697+bae-suho@users.noreply.github.com>
2025-02-26 17:29:43 +0900

    [level 0] Title: 배열 조각하기, Time: 0.36 ms, Memory: 20.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181893. 배열 조각하기/배열 조각하기.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+func solution(_ arr: [Int], _ query: [Int]) -> [Int] {
+    var result = arr
+    
+    for i in 0..<query.count {
+        if i % 2 == 0 {
+            result = Array(result[...(query[i])]) // query[i] 이후 버림
+        } else {
+            result = Array(result[query[i]...]) // query[i] 이전 버림
+        }
+    }
+    
+    return result
+}
