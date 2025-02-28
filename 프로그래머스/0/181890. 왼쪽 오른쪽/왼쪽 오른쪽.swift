215c0cc8548ea0f07953150c43a486522f35991c
bae-suho <115385697+bae-suho@users.noreply.github.com>
2025-02-28 10:01:17 +0900

    [level 0] Title: 왼쪽 오른쪽, Time: 0.08 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.swift
+++ 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.swift
@@ -3,19 +3,16 @@ import Foundation
 func solution(_ str_list:[String]) -> [String] {
     for i in 0..<str_list.count {
         if str_list[i] == "l" {
-            if i != 0 {
-                return Array(str_list[..<i])
+            if i == 0 {
+                return []
             }
             else {
-                return []
+                return Array(str_list[..<i])
             }
         }
         else if str_list[i] == "r" {
-            if i != str_list.count-1 {
-                return Array(str_list[(i+1)...])
-            }
-            else {
-                return []
+            if i == str_list.count-1 {
+                return Array(str_list[i...])
             }
         }
     }
