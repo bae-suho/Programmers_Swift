29c7c2b1ed6f5424432e78f7ded67d43a7ae9011
bae-suho <115385697+bae-suho@users.noreply.github.com>
2025-03-13 21:57:12 +0900

    [level 0] Title: A 강조하기, Time: 0.03 ms, Memory: 16.1 MB -BaekjoonHub

--- 프로그래머스/0/181874. A 강조하기/A 강조하기.swift
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.swift
@@ -1,5 +1,5 @@
 import Foundation
 
 func solution(_ myString:String) -> String {
-    return String(myString.map{ $0 == "a" || $0 == "A" ? $0.uppercased().first! : $0.lowercased().first! })
+    return String(myString.map{ $0 == "a" || $0 == "A" ? Character($0.uppercased()) : Character($0.lowercased()) })
 }
