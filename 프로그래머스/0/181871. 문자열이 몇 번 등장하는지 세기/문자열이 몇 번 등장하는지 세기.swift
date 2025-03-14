d3a9d6e05501dcbce9bf09bda31764157d8d022c
bae-suho <115385697+bae-suho@users.noreply.github.com>
2025-03-14 12:25:29 +0900

    [level 0] Title: 문자열이 몇 번 등장하는지 세기, Time: 44.90 ms, Memory: 16.6 MB -BaekjoonHub

--- 프로그래머스/0/181871. 문자열이 몇 번 등장하는지 세기/문자열이 몇 번 등장하는지 세기.swift
+++ 프로그래머스/0/181871. 문자열이 몇 번 등장하는지 세기/문자열이 몇 번 등장하는지 세기.swift
@@ -1,7 +1,5 @@
 import Foundation
 
 func solution(_ myString:String, _ pat:String) -> Int {
-    let patCount = pat.count
-    return Array(myString)[0...myString.count - patCount].enumerated().map{ (index, value) in return String(Array(myString)[index..<(index+patCount)]) 
-                                                                          }.filter{$0 == pat}.count
+    return (0...myString.count - pat.count).map{String(Array(myString)[$0..<($0 + pat.count)])}.filter{$0 == pat}.count
 }
