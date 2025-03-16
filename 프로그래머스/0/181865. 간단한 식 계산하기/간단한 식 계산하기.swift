55e4ddc59402a266201c3f967fb779c4a61213eb
bae-suho <115385697+bae-suho@users.noreply.github.com>
2025-03-16 21:59:36 +0900

    [level 0] Title: 간단한 식 계산하기, Time: 0.08 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/0/181865. 간단한 식 계산하기/간단한 식 계산하기.swift
+++ 프로그래머스/0/181865. 간단한 식 계산하기/간단한 식 계산하기.swift
@@ -5,11 +5,8 @@ func solution(_ binomial:String) -> Int {
     let (a, op, b) = (Int(components[0])!, components[1], Int(components[2])!)
     switch op {
         case "+": return a + b
-        break
         case "-": return a - b
-        break
         case "*": return a * b
-        break
         default : return -1
     }
 }
