1ef250d5ea500eb66aa3104c601a005ab5079add
bae-suho <115385697+bae-suho@users.noreply.github.com>
2025-03-17 20:27:16 +0900

    [level 0] Title: 빈 배열에 추가, 삭제하기, Time: 0.15 ms, Memory: 16.6 MB -BaekjoonHub

--- 프로그래머스/0/181860. 빈 배열에 추가， 삭제하기/빈 배열에 추가， 삭제하기.swift
+++ 프로그래머스/0/181860. 빈 배열에 추가， 삭제하기/빈 배열에 추가， 삭제하기.swift
@@ -4,7 +4,7 @@ func solution(_ arr:[Int], _ flag:[Bool]) -> [Int] {
     var x:[Int] = []
     for i in 0..<arr.count {
         if flag[i] {
-            x = x + (Array(repeating: arr[i] , count: arr[i] * 2))
+            x += (Array(repeating: arr[i] , count: arr[i] * 2))
         }
         else {
             x.removeLast(arr[i])
