4d8b5aca7cfce2cc25d74de8860be6a2a6c4e07a
bae-suho <115385697+bae-suho@users.noreply.github.com>
2025-03-18 14:06:23 +0900

    [level 0] Title: 배열 만들기 6, Time: 442.11 ms, Memory: 57.8 MB -BaekjoonHub

--- 프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.swift
+++ 프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.swift
@@ -3,14 +3,11 @@ import Foundation
 func solution(_ arr:[Int]) -> [Int] {
     var stk = Array<Int>()
     for i in 0..<arr.count{
-        if stk.isEmpty {
+        if stk.isEmpty || stk.last != arr[i] {
             stk += [arr[i]]
-        }
-        else if stk[stk.count-1] == arr[i] {
-            stk.removeLast()
         }
         else {
-            stk += [arr[i]]
+            stk.removeLast()
         }
     }
     return !stk.isEmpty ? stk : [-1]
