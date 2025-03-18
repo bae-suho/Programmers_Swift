13c992b6a69af4d6f46c63fa7e36cd7a9fc74531
bae-suho <115385697+bae-suho@users.noreply.github.com>
2025-03-18 17:25:06 +0900

    [level 0] Title: 문자열 묶기, Time: 98.27 ms, Memory: 24.7 MB -BaekjoonHub

--- 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.swift
+++ 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.swift
@@ -5,5 +5,5 @@ func solution(_ strArr:[String]) -> Int {
     for s in strArr {
         dict.updateValue(dict.keys.contains(s.count) ? dict[s.count]! + 1 : 1, forKey: s.count)
     }
-    return dict.values.sorted{$0 > $1}[0]
+    return dict.values.max()!
 }
