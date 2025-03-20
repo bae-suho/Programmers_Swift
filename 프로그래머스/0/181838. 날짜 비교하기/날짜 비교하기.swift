e53b241012a9cd7bf19f43a7f6615695bcade1c2
bae-suho <115385697+bae-suho@users.noreply.github.com>
2025-03-20 13:22:44 +0900

    [level 0] Title: 날짜 비교하기, Time: 0.07 ms, Memory: 16.6 MB -BaekjoonHub

--- 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.swift
+++ 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.swift
@@ -1,13 +1,5 @@
 import Foundation
 
 func solution(_ date1:[Int], _ date2:[Int]) -> Int {
-    for i in 0..<date1.count{
-        if date1[i]<date2[i] {
-            return 1
-        }
-        else if date1[i]>date2[i]{
-            return 0
-        }
-    }
-    return 0
+    return Int(date1.map{String($0)}.joined())! < Int(date2.map{String($0)}.joined())! ? 1 : 0
 }
