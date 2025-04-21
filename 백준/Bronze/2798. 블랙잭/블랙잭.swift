f7200643a04b1e069bb4e9e4a63fafa6aaa0c77c
bae-suho <115385697+bae-suho@users.noreply.github.com>
2025-04-21 17:20:57 +0900

    [Bronze II] Title: 블랙잭, Time: 8 ms, Memory: 69104 KB -BaekjoonHub

---
+++ 백준/Bronze/2798. 블랙잭/블랙잭.swift
@@ -0,0 +1,25 @@
+// 5 21
+// 5 6 7 8 9
+
+let input = readLine()!.split(separator: " ").map{Int($0)!}
+let (n, m) = (input[0], input[1])
+let cards = readLine()!.split(separator: " ").map{Int($0)!}
+var result = 0
+
+for i in 0..<(n-2) {
+    for j in (i + 1)..<(n-1) {
+        for k in (j + 1)..<n {
+            let sum = cards[i] + cards[j] + cards[k]
+            if sum == m {
+                result = sum
+                break
+            }
+            else if result < sum && sum <= m {
+                result = sum    
+                } 
+            
+        }
+    }
+}
+
+print(result)
